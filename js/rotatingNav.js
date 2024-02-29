0cffc83708f4470a9749e473e6f6e3d6335804dc
Ruthachere <akoruth22@gmail.com>
2024-02-29 12:02:17 +0100

    Exercises on blur loading and scroll animation

--- js/rotatingNav.js
+++ js/rotatingNav.js
@@ -1,3 +1,4 @@
+//JS CODE FOR ROTATING NAVIGATION
 const open = document.querySelector('.open-btn');
 const close = document.querySelector('.close-btn');
 const container = document.querySelector('.container');
@@ -5,3 +6,13 @@ const container = document.querySelector('.container');
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+// JS CODE FOR HIDDEN SEARCH WIDGET
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus();
+})
