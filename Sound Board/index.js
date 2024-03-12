2f6f7426ca9efee43df1a5f7510736d056aa47ab
Ruthachere <akoruth22@gmail.com>
2024-03-12 20:56:36 +0100

    modifications on sound board and exercise on random choice picker

--- Sound Board/index.js
+++ Sound Board/index.js
@@ -1,23 +1,23 @@
-const sounds = ['piano', 'applauase', 'glory', 'clap', 'speech', 'footStep']
 
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
 sounds.forEach(sound => {
-    const btn = document.createElement('button');
+    const btn = document.createElement('button')
     btn.classList.add('btn');
 
     btn.innerText = sound;
-    
+
     btn.addEventListener('click', () => {
         stopSongs();
+
         document.getElementById(sound).play();
-        
     })
 
     document.getElementById('buttons').appendChild(btn);
-});
+})
 
 function stopSongs() {
-    sounds.forEach(sound =>{
+    sounds.forEach(sound => {
         const song = document.getElementById(sound);
 
         song.pause();
